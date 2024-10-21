e723e3133e17eebfd12312e1a68a915cf680d58d
MiguelIglesiasAbarca <125640186+MiguelIglesiasAbarca@users.noreply.github.com>
2024-10-21 09:52:17 +0200

    UI (lifes & score)
    Added a gameloop, with 5 lives and a score.

--- ArkanoidMIA/Assets/Scripts/BouncyBall.cs
+++ ArkanoidMIA/Assets/Scripts/BouncyBall.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BouncyBall : MonoBehaviour
 {
     public float minY = -5.5f;
     public float maxVelocity = 15f;
     Rigidbody2D rb;
+
+    int score = 0;
+    int lives = 5;
+
+    public TextMeshProUGUI scoreTxt;
+    public GameObject[] livesImage;
     
     // Start is called before the first frame update
     void Start()
@@ -19,8 +26,19 @@ public class BouncyBall : MonoBehaviour
     {
         if(transform.position.y < minY)
         {
-            transform.position = Vector3.zero;
-            rb.velocity = Vector3.zero;
+            if(lives <= 0)
+            {
+                GameOver();
+            }
+            else
+            {
+                transform.position = Vector3.zero;
+                rb.velocity = Vector3.zero;
+
+                lives--;
+                livesImage[lives].SetActive(false);
+            }
+            
         }
 
         if(rb.velocity.magnitude > maxVelocity)
@@ -34,6 +52,14 @@ public class BouncyBall : MonoBehaviour
         if(collision.gameObject.CompareTag("Brick"))
         {
             Destroy(collision.gameObject);
+
+            score +=10;
+            scoreTxt.text = score.ToString("00000");
         }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GameOver");
     }
 }
